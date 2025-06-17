d925285442dcac7ed1acb48079d32e5d72c5b3de
ignacio <ig.mugonz@gmail.com>
2025-06-16 21:30:20 -0400

    cambio de sonarqube, se agrega log

--- src/main/java/com/equipo/saludo/App.java
+++ src/main/java/com/equipo/saludo/App.java
@@ -1,9 +1,14 @@
 package com.equipo.saludo;
+import java.util.logging.Logger;
+
 public class App {
  public static String saludar(String nombre) {
  return "¡Hola, " + nombre + "!";
  }
  public static void main(String[] args) {
- System.out.println(saludar("DevOps"));
+  Logger logger = Logger.getLogger(App.class.getName());
+  if (logger.isLoggable(java.util.logging.Level.INFO)) {
+   logger.info(saludar("DevOps"));
+  }
  }
 }
